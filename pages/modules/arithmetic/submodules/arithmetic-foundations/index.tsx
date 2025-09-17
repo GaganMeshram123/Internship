5ebd8dd9a54f9a128b568ebdef08f8bcdb366188
Gagan Hariram Meshram <109589418+GaganMeshram123@users.noreply.github.com>
2025-09-17 07:06:28 +0000

    updated slide 3

--- pages/modules/arithmetic/submodules/arithmetic-foundations/index.tsx
+++ pages/modules/arithmetic/submodules/arithmetic-foundations/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Slide } from '../../../common-components/concept';
 
 // You will create these component files for each slide in this folder
-import CountingSlide from './Slide1';/* 
-import AdditionSlide from './Slide2';
-import SubtractionSlide from './Slide3';
+import CountingSlide from './Slide1';  
+import AdditionSlide from './Slide2';  
+import SubtractionSlide from './Slide3'; /*
 import MultiplicationSlide from './Slide4';
-import DivisionSlide from './Slide5'; */
+import DivisionSlide from './Slide5';   */
 
 // This array defines the order of the slides in this submodule.
 export const arithmeticFoundationsSlides: Slide[] = [
@@ -16,19 +16,19 @@ export const arithmeticFoundationsSlides: Slide[] = [
     component: CountingSlide,
     id: 'arithmetic-counting'
   },
-  /* {
+    {
     type: 'interactive',
     title: 'Fun with Addition: Putting Things Together',
     component: AdditionSlide,
     id: 'arithmetic-addition'
   },
-  {
+   {
     type: 'interactive',
     title: 'Super Subtraction: Taking Things Away',
     component: SubtractionSlide,
     id: 'arithmetic-subtraction'
   },
-  {
+   /*{
     type: 'interactive',
     title: 'Amazing Multiplication: Adding in Groups',
     component: MultiplicationSlide,
@@ -39,7 +39,7 @@ export const arithmeticFoundationsSlides: Slide[] = [
     title: 'Dazzling Division: Sharing Fairly',
     component: DivisionSlide,
     id: 'arithmetic-division'
-  } */
+  }  */
 ];
 
 function ArithmeticFoundationsSubmodule() {
